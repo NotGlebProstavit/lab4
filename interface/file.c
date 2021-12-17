1f2a68113330d4a413cdddb83ef5a2169dfaa214
Pustovit Vladislav <pvs013@campus.mephi.ru>
2021-12-17 21:05:56 +0300

    :JGF1

--- interface/file.c
+++ interface/file.c
@@ -16,7 +16,7 @@ void fileInput(Log** logs, int* n){
         *n = 0;
 		return;
 	}
-	if(*logs != NULL) freeAll(*logs, *n);
+	if(*logs != NULL) freeAll(logs, *n);
 
 	while(!feof(fm)){
 		if(!freadline(fm)) break;
